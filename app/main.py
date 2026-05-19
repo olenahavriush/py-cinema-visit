ea0c114928a27e4e0597afbcf51d7d60980d65cb
Oleksii Havriushenko <metropol@ukr.net>
2026-05-19 13:09:00 +0300

    autotests

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
     new_customers = list[Customer]()
     clean = Cleaner(cleaner)
     cinema = CinemaHall(hall_number)
--- app/people/customer.py
+++ app/people/customer.py
@@ -2,5 +2,6 @@ class Customer:
     def __init__(self, name: str, food: str) -> None:
         self.name = name
         self.food = food
+
     def watch_movie(self, movie: str) -> None:
-       print(f'{self.name} is watching "{movie}".')
+        print(f'{self.name} is watching "{movie}".')
